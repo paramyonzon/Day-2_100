086c353cb07aa447b70f243b872f2e043d2c5044
Param Yonzon <param.yonzon14@stjohns.edu>
2022-12-25 02:27:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to day 2's challenge! Congrats on making it this far. Keep on going! :)")
+print()
+userName = input("Your name: ")
+userFavoriteFood = input("What's your favorite music? ")
+userLocation = input ( "Where do you live? ")
+
+print("your name is", userName)
+print ("your favorite food is", userFavoriteFood)
+print ("Your live location is", userLocation)
+
